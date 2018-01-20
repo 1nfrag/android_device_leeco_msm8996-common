aba13a6a82ee27de4a2643ae64c9adffa23c0793
dianlujitao <dianlujitao@lineageos.org>
2018-01-20 18:40:23 +0800

    msm8996: Add powerhint.xml
     * Based on LA.UM.6.5.r1-05300-8x96.0, adapted for the new format.
     * Required by perf_hint in libqti-perfd-client.so.
    
    Change-Id: Ibcd7eb642622d91a5634b6abd3bae3a386c96bc6

--- msm8996.mk
+++ msm8996.mk
@@ -342,6 +342,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # Qualcomm
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-oem.xml:system/etc/permissions/privapp-permissions-oem.xml \
